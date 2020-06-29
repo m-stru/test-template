656ecb82097109686ebf324c643a4fd8aae1ad22
Stru <a.struzhkova@gmail.com>
2020-06-29 04:45:45 +0300

    mobile breakpoints, slider

---
+++ scripts/menuscript.js
@@ -0,0 +1,5 @@
+let menu = document.getElementById('menu');
+let menuToggle = document.getElementById('toggle');
+menuToggle.addEventListener('click', function () {
+    menu.classList.toggle('show');
+});
